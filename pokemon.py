b255c752bdb17fd415a9f187a39290fef9ccd223
Ryan McCormick <rmccorm4@binghamton.edu>
2017-03-31 23:13:18 -0500

    Get stats

--- pokemon.py
+++ pokemon.py
@@ -17,6 +17,13 @@ nameLine = soup.find(string=re.compile("#"+pokedex_number))
 pokemon_name = nameLine[:nameLine.find(" ")]
 print(pokemon_name)
 
+baseStatsTotal = soup.find(string=re.compile("Base Stats - Total:"))
+print(baseStatsTotal)
+
+baseStats = soup.find_all(class_="fooinfo")
+speedLine = baseStats[len(baseStats)-16]
+speed = speedLine[speedLine.find(">")+1:speedLine.find("/")-1]
+
 base_image_url = "http://www.serebii.net/xy/pokemon/"
 full_image_url = base_image_url + pokedex_number + ".png"
 
