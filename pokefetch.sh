78c98a6e6904823bd363e3705b55992175bdd13d
Ryan McCormick <rmccorm4@binghamton.edu>
2017-05-26 15:03:40 -0400

    try column command

--- pokefetch.sh
+++ pokefetch.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 /usr/bin/env python3 pokefetch.py $@
 catimg -w 120 "imgs/Pikachu.png"
+# Try this out:
+# catimg -w 120 "imgs/Pikachu.png" | column -t
+
 #Use catimg to print pixel art, width of 200 pixels is good balance of size/quality
 
