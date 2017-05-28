755cb30203907c593c358d0a3ffd414bd5ba6855
Ryan McCormick <rmccorm4@binghamton.edu>
2017-05-28 18:19:28 -0500

    finally figured out how to do side by side

--- pokefetch.sh
+++ pokefetch.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
-/usr/bin/env python3 pokefetch.py $@
-catimg -w 120 "imgs/Pikachu.png"
-#Use catimg to print pixel art, width of 200 pixels is good balance of size/quality
 
+# Prepend 10 blank lines for nice format
+echo > stats.txt
+for ((i=0; i<9; i++));
+do
+	echo >> stats.txt
+done
+
+# Get pokemon stats and output to file
+/usr/bin/env python3 pokefetch.py $@ >> stats.txt
+
+# Format input for image
+POKEMON=$@                # '$@' refers to command line argument
+POKEMON=${POKEMON[@]^}    # This converts to Title Case
+
+
+# Get image and output to file
+catimg -w 120 "imgs/$POKEMON.png" > image.txt
+
+# Print image and stats side by side
+pr -mts image.txt stats.txt
